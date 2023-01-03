2e4d8f1887d51d221fc1dba0840509a83b3e023c
CountNeuroman <olorinoren@gmail.com>
2023-01-03 21:03:17 +0300

    Add Breadth First Search example

---
+++ BreadthFirstSearch/BreadthFirstSearch/Program.cs
@@ -0,0 +1,45 @@
+﻿
+namespace BreadthFirstSearch;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        
+        Dictionary<string, string[]> friendsGraph = new()
+        {
+            { "You", new[] { "Alice", "Bob", "Claire" } },
+            { "Bob", new[] { "Anuj", "Peggy" } },
+            { "Alice", new []{"Peggy"} },
+            { "Claire", new []{"Thom", "Jonnys"} },
+            { "Anuj", Array.Empty<string>() },
+            { "Peggy", Array.Empty<string>() },
+            { "Thom", Array.Empty<string>() },
+            { "Jonnys", Array.Empty<string>() }
+        };
+        string[] myFriends = friendsGraph["You"];
+        var searched = new HashSet<string>();
+
+        Queue<string> queue = new(myFriends);
+
+        while (queue.Count > 0)
+        {
+            string name = queue.Dequeue();
+            if (searched.Contains(name))
+                continue;
+            else
+            {
+                searched.Add(name);
+                if (name[^1] == 'm')
+                {
+                    Console.WriteLine($"{name} is seller mango!");
+                    
+                    break;
+                }
+            }
+
+            var friends = friendsGraph[name].ToList();
+            friends.ForEach(x => queue.Enqueue(x));
+        }
+    }
+}
