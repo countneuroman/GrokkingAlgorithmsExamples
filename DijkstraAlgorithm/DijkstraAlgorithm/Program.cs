f196133baae3aedb9f03ca2674659f9d439ac80f
CountNeuroman <olorinoren@gmail.com>
2023-01-06 21:07:20 +0300

    Add Dijkstra Algorithm

---
+++ DijkstraAlgorithm/DijkstraAlgorithm/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace DijkstraAlgorithm;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<string, Dictionary<string, int>> weighetedGraph = new()
+        {
+            { "start", new Dictionary<string, int>() { {"a", 6}, {"b", 2} } },
+            { "a", new Dictionary<string, int>() { {"finish", 1} } },
+            { "b", new Dictionary<string, int>() { {"a", 3}, {"finish", 5} } },
+            { "finish", new Dictionary<string, int>() }
+        };
+
+        Dictionary<string, int> costs = new()
+        {
+            {"a", 6},
+            {"b", 2},
+            {"finish", int.MinValue} //MinValue represents infinity.
+        };
+
+        Dictionary<string, string> parents = new()
+        {
+            {"a", "start"},
+            {"b", "start"},
+            {"finish", string.Empty}
+        };
+
+        HashSet<string> processed = new();
+
+        GetShortAndLowCostPath(weighetedGraph, costs, parents, processed);
+    }
+
+    private static void GetShortAndLowCostPath(Dictionary<string, Dictionary<string, int>> weighetedGraph,
+        Dictionary<string, int> costs, Dictionary<string, string> parents, HashSet<string> processed)
+    {
+        string lowCostNode = FindLowestCostNode(costs, processed);
+        while (lowCostNode != string.Empty)
+        {
+            int cost = costs[lowCostNode];
+            Dictionary<string, int> neighbors = weighetedGraph[lowCostNode];
+            foreach (var neighbor in neighbors)
+            {
+                int newCost = cost + neighbor.Value;
+                if (costs[neighbor.Key] > newCost)
+                {
+                    costs[neighbor.Key] = newCost;
+                    parents[neighbor.Key] = lowCostNode;
+                }
+            }
+
+            processed.Add(lowCostNode);
+            lowCostNode = FindLowestCostNode(costs, processed);
+        }
+    }
+
+    private static string FindLowestCostNode(Dictionary<string, int> costs, HashSet<string> processed)
+    {
+        string minimalNode = costs.Where(x => x.Value >= 0).MinBy(x=>x.Value).Key;
+        if (processed.Contains(minimalNode))
+            return "";
+        return minimalNode;
+    }
+}
